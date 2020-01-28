5df8c924b3349dfbf715240cf863c51e850eba53
Willian Galvani <williangalvani@gmail.com>
2020-01-28 12:46:17 -0300

    update swagger.js to openapi 3.0
    Co-authored-by: Willian Galvani <williangalvani@gmail.com>
    Co-authored-by: Patrick José Pereira <patrickelectric@gmail.com>

--- configuration/swagger.js
+++ configuration/swagger.js
@@ -1,20 +1,17 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 
-const swaggerDefinition = {
-  info: {
-    title: 'REST API for my App', // Title of the documentation
-    version: '1.0.0', // Version of the app
-    description: 'This is the REST API for my product', // short description of the app
-  },
-  host: 'localhost:3000', // the host or url of the app
-  basePath: '/api', // the basepath of your endpoint
-};
-
 // options for the swagger docs
 const options = {
   // import swaggerDefinitions
-  swaggerDefinition,
+  definition: {
+    openapi: '3.0.0', // Specification (optional, defaults to swagger: '2.0')
+    info: {
+      title: 'Companion', // Title (required)
+      version: '1.0.0', // Version (required)
+    },
+  },
   // path to the API docs
+  host: 'localhost:3000', // the host or url of the app
   apis: ['./docs/*.yaml'],
 };
 // initialize swagger-jsdoc
